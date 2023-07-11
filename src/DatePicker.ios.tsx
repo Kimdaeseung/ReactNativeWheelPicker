b527d63c6e6ef258a1f499b8d4c52270264d48aa
daeseung Kim <eovkf2040@gmail.com>
2023-07-11 16:18:07 +0900

    only Wheel Pick Update

--- src/DatePicker.ios.tsx
+++ src/DatePicker.ios.tsx
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
-import { View } from "react-native";
-import RNDateTimePicker from "@react-native-community/datetimepicker"
+// import React, { useState } from "react";
+// import { View } from "react-native";
+// import RNDateTimePicker from "@react-native-community/datetimepicker"
 
-interface Props {
-  initDate: Date;
-  onDateSelected?: Function;
-  disabled?: boolean;
-}
+// interface Props {
+//   initDate: Date;
+//   onDateSelected?: Function;
+//   disabled?: boolean;
+// }
 
-const DatePicker: React.FC<Props> = props => {
-  const { initDate, onDateSelected, disabled } = props;
-  const [date, setDate] = useState(initDate || new Date())
-  return (
-    <View pointerEvents={disabled ? "none" : "auto"}>
-      <RNDateTimePicker
-        value={date}
-        onChange={(event, date) => {
-          if (onDateSelected) onDateSelected(date)
-          setDate(date)
-        }}
-        mode="date"
-        {...props}
-      />
-    </View>
-  );
-};
+// const DatePicker: React.FC<Props> = props => {
+//   const { initDate, onDateSelected, disabled } = props;
+//   const [date, setDate] = useState(initDate || new Date())
+//   return (
+//     <View pointerEvents={disabled ? "none" : "auto"}>
+//       <RNDateTimePicker
+//         value={date}
+//         onChange={(event, date) => {
+//           if (onDateSelected) onDateSelected(date)
+//           setDate(date)
+//         }}
+//         mode="date"
+//         {...props}
+//       />
+//     </View>
+//   );
+// };
 
-export default DatePicker;
+// export default DatePicker;
